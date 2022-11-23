cf7a30c8329084551183dbe1a0f6b933b41d52fc
Coty Seth <sethcm22@gmail.com>
2022-11-23 14:09:38 -0500

    restored files

---
+++ README.md
@@ -0,0 +1,1 @@
+A memory card inspired poker game where players attempt to draft the best poker hand possible.
--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -6,16 +6,16 @@ import { seed } from "../seedCards";
 export default function Gameboard() {
   const [cards, setCards] = useState(seed);
 
-  //   useEffect(() => {
-  //     async function getData() {
-  //       const response = await axios.get(
-  //         "https://deckofcardsapi.com/api/deck/new/draw/?count=15"
-  //       );
-  //       console.log(response.data.cards);
-  //       setCards(response.data.cards);
-  //     }
-  //     getData();
-  //   }, []);
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(
+        "https://deckofcardsapi.com/api/deck/new/draw/?count=15"
+      );
+      console.log(response.data.cards);
+      setCards(response.data.cards);
+    }
+    getData();
+  }, []);
   return (
     <div className="Gameboard">
       <Drawpile cards={cards} />
